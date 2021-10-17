9dcebd9964202c3a3fb150def3d9f1b92afa9b68
Linh Tran <tranchilinh0312@gmail.com>
2021-10-17 23:19:13 +0700

    add product detail component in product admin page

--- client/src/components/Admin/Product.js
+++ client/src/components/Admin/Product.js
@@ -10,6 +10,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableContainer from '@material-ui/core/TableContainer';
 import OpacityIcon from '@material-ui/icons/Opacity';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import { Card, CardMedia, CardActions, CardActionArea, Divider } from "@material-ui/core";
 import { Box, Typography, Grid, IconButton, Button, TextField, FormControl, Select, InputLabel, MenuItem } from "@material-ui/core";
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import Rating from "../Rating";
@@ -19,7 +21,15 @@ const useStyles = makeStyles({
     },
     fullWidth: {
         width: "100%"
-    }
+    },
+    img: {
+        maxWidth: 245,
+        width: "100%",
+
+    },
+    media: {
+        height: 140,
+    },
 });
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -141,8 +151,8 @@ export default function ProductAdmin() {
                             </Table>
                         </TableContainer>
                     </Grid>
-                    <Grid item container xs={12}>
-                        <Grid item xs={6}>
+                    <Grid item container xs={12} spacing={2}>
+                        <Grid item sm={6} xs={12}>
                             <Box>
                                 <Button color="primary" fullWidth variant="contained">Add new Product</Button>
                                 <Box my={5}>
@@ -150,7 +160,9 @@ export default function ProductAdmin() {
                                         <TextField type="text" fullWidth label="Product name" variant="outlined" value="Zara" />
                                     </Box>
                                     <Box my={2}>
-                                        <FormControl variant="outlined" className={classes.fullWidth}>
+                                        <Grid container spacing={1}>
+                                            <Grid item xs={6}>
+                                            <FormControl variant="outlined" className={classes.fullWidth}>
                                             <InputLabel id="demo-simple-select-outlined-label">Category</InputLabel>
                                             <Select
                                                 labelId="demo-simple-select-outlined-label"
@@ -168,9 +180,9 @@ export default function ProductAdmin() {
                                                 }
                                             </Select>
                                         </FormControl>
-                                    </Box>
-                                    <Box my={2}>
-                                        <FormControl variant="outlined" className={classes.fullWidth}>
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                            <FormControl variant="outlined" className={classes.fullWidth}>
                                             <InputLabel id="demo-simple-select-outlined-label">Brand</InputLabel>
                                             <Select
                                                 labelId="demo-simple-select-outlined-label"
@@ -188,9 +200,15 @@ export default function ProductAdmin() {
                                                 }
                                             </Select>
                                         </FormControl>
+                                            </Grid>
+                                        </Grid>
+                                       
                                     </Box>
                                     <Box my={2}>
-                                    <TextField type="text" fullWidth label="Price" variant="outlined" value="200000" />
+                                    <TextField type="text" multiline rows={4} fullWidth label="Summary" variant="outlined"   />
+                                    </Box>
+                                    <Box my={2}>
+                                        <TextField type="text" fullWidth label="Price" variant="outlined" value="200000" />
                                     </Box>
                                     <Box my={2}>
                                         <Grid container spacing={2}>
@@ -205,8 +223,136 @@ export default function ProductAdmin() {
                                 </Box>
                             </Box>
                         </Grid>
-                        <Grid item xs={6}>
+                        <Grid item sm={6} xs={12}>
+                            <Box fullWidth p={1} border={1} sx={{ borderColor: "grey" }}>
+                                <Box sx={{ height: '100%', height: 400, overflow: "auto" }}>
+                                    <Grid container spacing={3} style={{
+                                        margin: 0,
+                                        width: '100%',
+                                    }}>
+                                        {
+                                            [0, 1, 3, 4, 5].map(item => (
+                                                <Grid key={item} item md={4} xs={6}>
+                                                    <Card className={classes.img}>
+                                                        <CardActionArea>
+                                                            <CardMedia
+                                                                className={classes.media}
+                                                                image="https://image.vietnamnews.vn/uploadvnnews/Article/2021/3/18/142705_hoa.jpg"
+                                                                title="Contemplative Reptile"
+                                                            />
+                                                        </CardActionArea>
+                                                        <CardActions >
+                                                            <Button fullWidth size="small" color="secondary">
+                                                                <DeleteOutlineIcon />
+                                                            </Button>
+                                                        </CardActions>
+                                                    </Card>
+                                                </Grid>
+                                            ))
+                                        }
+                                    </Grid>
+                                </Box>
+                                <Box my={1}>
+                                    <Button variant="outlined" color="primary" fullWidth>Add Image</Button>
+                                </Box>
+                            </Box>
                         </Grid>
+                    </Grid>
+                </Grid>
+
+                <Box py={5}>
+                <Divider />
+                </Box>
+              
+                <Grid container spacing={2}>
+                    <Grid item sm={6} xs={12}>
+                        <Box my={2}>
+                            <Typography variant="h6">Ao vest tong nam</Typography>
+                        </Box>
+                        <TableContainer component={Paper}>
+                            <Table aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell  ><strong>No</strong></TableCell>
+                                        <TableCell  ><strong>Color</strong></TableCell>
+                                        <TableCell  ><strong>Size</strong></TableCell>
+                                        <TableCell  ><strong>Quantity</strong></TableCell>
+                                        <TableCell  ><strong></strong></TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {rowsCategories.map((row, index) => (
+                                        <TableRow key={row.name}>
+                                            <TableCell component="th" scope="row">
+                                                {`${index + 1}`}
+                                            </TableCell>
+                                            <TableCell  >Blue</TableCell>
+                                            <TableCell  >XS</TableCell>
+                                            <TableCell  >29</TableCell>
+                                            <TableCell  ><IconButton size="small"><MoreHorizIcon /></IconButton></TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                    <Grid item sm={6} xs={12}>
+                        <Box>
+                            <Button color="primary" fullWidth variant="contained">Add new Variant</Button>
+                            <Box my={5}>
+                                <Box my={2}>
+                                    <FormControl variant="outlined" className={classes.fullWidth}>
+                                        <InputLabel id="demo-simple-select-outlined-label">Color</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-outlined-label"
+                                            id="demo-simple-select-outlined"
+                                            value="Zara"
+
+                                            label="Color"
+                                            name="color"
+                                        >
+                                            <MenuItem value="Zara">
+                                                Violet
+                                            </MenuItem>
+                                            {
+                                                rowsBrands.map(row => <MenuItem value={row.id}>{row.name}</MenuItem>)
+                                            }
+                                        </Select>
+                                    </FormControl>
+                                </Box>
+                                <Box my={2}>
+                                    <FormControl variant="outlined" className={classes.fullWidth}>
+                                        <InputLabel id="demo-simple-select-outlined-label">Size</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-outlined-label"
+                                            id="demo-simple-select-outlined"
+                                            value="Zara"
+
+                                            label="Size"
+                                            name="size"
+                                        >
+                                            <MenuItem value="Zara">
+                                                XXLl
+                                            </MenuItem>
+                                            {
+                                                rowsBrands.map(row => <MenuItem value={row.id}>{row.name}</MenuItem>)
+                                            }
+                                        </Select>
+                                    </FormControl>
+                                </Box>
+                                <TextField type="text" fullWidth label="Quantity" variant="outlined" value="20" />
+                                <Box my={2}>
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={6}>
+                                            <Button fullWidth color="secondary" variant="contained">Delete</Button>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Button color="primary" fullWidth variant="contained">Save</Button>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            </Box>
+                        </Box>
                     </Grid>
                 </Grid>
             </TabPanel>
@@ -417,9 +563,7 @@ export default function ProductAdmin() {
                                         </Grid>
                                     </Grid>
                                 </Box>
-
                             </Box>
-
                         </Box>
                     </Grid>
                 </Grid>
